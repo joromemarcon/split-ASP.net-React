83ffab02eb4d1bd0e5ccebc628a626d32ebfe4f9
Jorome Marcon <joromemarcon@gmail.com>
2024-09-27 18:20:40 -0700

    Creating dto for Item model. Adding GETALL and GetById Requests for Item Model.

---
+++ Repository/ItemRepository.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using split_api.Data;
+using split_api.Interfaces;
+using split_api.Models;
+
+namespace split_api.Repository
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly DataContext _context;
+        public ItemRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Item>> GetAllItemAsync()
+        {
+            return await _context.Items.ToListAsync();
+        }
+
+        public async Task<Item?> GetItemByIdAsync(int id)
+        {
+            return await _context.Items.FindAsync(id);
+        }
+    }
+}
